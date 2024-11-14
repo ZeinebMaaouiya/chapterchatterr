da4a9562b2e611e2a91cfe7153ca21e54a7254ab
ZeinbMaaouiya <zeineboumaaouiya@gmail.com>
2024-11-14 19:54:21 +0100

    first commit

---
+++ app/Views/acceille.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" 
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+        <link rel="stylesheet" href="https://fontawesome.com/start">
+    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" href="/images/favicon/icon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon.png">
+    <link rel="manifest" href="/manifest/app.json">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.1/css/all.css">
+    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('js/main.css') ?>">
+</head>
+<body>
+    <header>
+        <h1>chapter<span>Chatter</span></h1>
+        <div class="divIcone">
+            <input type="search" placeholder=" search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div >
+            <p>signe in</p>
+            <i class="fa-solid fa-user"></i>
+        </div>
+    </header>
+    <main>
+        <h1> what will you discover ?</h1>
+        <div>
+            <div>
+                <p>Because meagan liked...</p>
+                <p> she discoverd:</p>
+            </div>
+            <ul>
+                <li><a href=""><img src="<?=base_url('/img/ART/how.to.sing.a.song.jpg')?>" alt=""></a></li>
+                <li><a href=""><img src="<?=base_url('/img/ART/200982318._SX318_.jpg')?>" alt=""></a></li>
+                <li><a href=""><img src="<?=base_url('/img/ART/lesser.ruins.a.novel.jpg')?>" alt=""></a></li>
+                <li><a href=""><img src="<?=base_url('/img/ART/taylor.swift.style.jpg')?>" alt=""></a></li>
+                <li><a href=""><img src="<?=base_url('/img/ART/the.art.of.crime.jpg')?>" alt=""></a></li>
+                <li><a href=""><img src="<?=base_url('/img/ART/the.book.of.purrs.jpg')?>" alt=""></a></li>
+                <li><i class="fa-solid fa-right"></i></li>
+                <li><a href=""><img src="<?=base_url('/img/ART/vivienne.jpg')?>" alt=""></a></li>
+            </ul>
+        </div>
+        <div>
+            <div>
+                <p>Because meagan liked...</p>
+                <p> she discoverd:</p>
+            </div>
+            <ul>
+                <li><a href=""><img src="/img/HORROR/absolution.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/HORROR/I.will.be.waiting.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/HORROR/the.book.of.witching.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/HORROR/the.book.of.witching.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/HORROR/model.home.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/HORROR/if.i.stopped.haunting.you.jpg" alt=""></a></li>
+                <li><i class="fa-solid fa-right"></i></li>
+                <li><a href=""><img src="/img/HORROR/the.bog.wife.jpg" alt=""></a></li>
+            </ul>
+        </div>
+        <div>
+            <div>
+                <p>Because meagan liked...</p>
+                <p> she discoverd:</p>
+            </div>
+            <ul>
+                <li><a href=""><img src="/img/PSYCHOLOGY/american.scary.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/PSYCHOLOGY/how.to.date.a.foreigner.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/PSYCHOLOGY/dopamine.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/PSYCHOLOGY/how.to.winter.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/POETRY/deer.run.home.jpg" alt=""></a></li>
+                <li><a href=""><img src="/img/POETRY/eat.the.world.jpg" alt=""></a></li>
+                <li><i class="fa-solid fa-right"></i></li>
+                <li><a href=""><img src="/img/POETRY/she.followed.the.moon.back.to.herself.jpg" alt=""></a></li>
+            </ul>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
